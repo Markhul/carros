809139b7efed6fec5fd3cbd812202adff5d76420
Markhul <marcosneves.ie@gmail.com>
2025-03-10 18:01:40 +0000

    Update views.py

--- cars/views.py
+++ cars/views.py
@@ -8,35 +8,6 @@ from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
-
-""" def cars_view(request):
-    cars = Car.objects.all().order_by('model')
-    search = request.GET.get('search')
-    
-    if search:
-        cars = Car.objects.filter(model__icontains=search).order_by('model')
-
-    return render(
-        request,
-        'cars.html',
-        {'cars': cars },
-    )  """
-
-""" class CarsView(View):
-
-    def get(self, request):
-        cars = Car.objects.all().order_by('model')
-        search = request.GET.get('search')
-        
-        if search:
-            cars = cars.filter(model__icontains=search)
-
-        return render(
-            request,
-            'cars.html',
-            {'cars': cars },
-        )  """
-    
 class CarsView(ListView):
     model = Car
     template_name = 'cars.html'
@@ -49,30 +20,6 @@ class CarsView(ListView):
             cars = cars.filter(model__icontains=search)
         return cars
 
-
-""" def new_car_view(request):
-    if request.method == 'POST':
-        new_car_form = CarModelForm(request.POST, request.FILES)
-        if new_car_form.is_valid():
-            new_car_form.save()
-            return redirect('cars_list')
-    else:
-        new_car_form = CarModelForm()
-    return render(request, 'new_car.html', {'new_car_form': new_car_form }) """
-
-""" class NewCarView(View):
-
-    def get(self, request):
-        new_car_form = CarModelForm()
-        return render(request, 'new_car.html', {'new_car_form': new_car_form })
-    
-    def post(self, request):
-        new_car_form = CarModelForm(request.POST, request.FILES)
-        if new_car_form.is_valid():
-            new_car_form.save()
-            return redirect('cars_list')
-        return render(request, 'new_car.html', {'new_car_form': new_car_form })
- """
 
 @method_decorator(login_required(login_url='login'), name='dispatch')
 class NewCarCreateView(CreateView):
